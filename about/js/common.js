40df84e6f2ac8fa68e58c9638a6d089f6c7212ed
songjian <songjianzaina@163.com>
2022-07-01 18:22:39 +0800

    Site updated: 2022-07-01 18:22:38

---
+++ about/js/common.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: " 民 間 手 艺 人 ",
+        background_image: 'img/66624.jpg',
+        avatar: 'img/tuling.jpg',
+        str1: " 我 是 碼 农 ",
+        str2: " 也 是 民 間 手 艺 人 ",
+        str3: " 我 是 程 序 猿 ",
+        str4: " 也 是 艺 術 家 ",
+        about_p1: '我是一个爱编程，爱音乐，爱运动，唯独不爱说话的程序猿！',
+        about_p2: '嘿嘿嘿，下面这张图是我随便找的，我们程序猿的祖师爷！',
+        // 技能 和 百分比
+        skill1: 'IT技能',
+        skill1_num: '70%',
+        skill2: '音乐制作',
+        skill2_num: '60%',
+        skill3: '历史',
+        skill3_num: '30%',
+        skill4: '投资',
+        skill4_num: '85%',
+        skill5: 'KONG FU 和养生',
+        skill5_num: '90%',
+        skill6: '外语',
+        skill6_num: '55%',
+        work_years: 7, // 工作年限
+        project_num: 16, // 项目总数
+        code_line_num: 666666,// 总代码量,
+        school1: '南昌大学', // 学校1
+        major1: '临床医学', // 专业1
+        major_dtl1: '主要研究人体生命活动, 正常与非正常现象的捕捉 潜在与非潜在隐患的预防及治疗 永葆青春秘密的探索和实现', // 简介1
+        school2: '有瓣教育学院', // 学校2
+        major2: '哲学', // 专业2
+        major_dtl2: '每一个人在本性上都想求知。思考和研究 扬长避短', // 简介2
+        years1: '2012 - 2015',
+        years2: '2016 - 终身',
+        job1: '研发',// 职位
+        jobp1: '疑难杂症妙手回春', // 工作简介
+        cpn1: 'Google Labs', // 公司
+        cpnyear1: '2015 - 2021',
+        job2: '音乐制作',
+        jobp2: '从0到1 Jut Do It',
+        cpn2: 'YouBan Studio',
+        cpnyear2: '2017 - 终身'
+    }
+})
